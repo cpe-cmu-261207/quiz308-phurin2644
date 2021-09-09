6fc889d7a1cbfa1d0404b64567fd51f06aba537d
phurin <76035456+phurin2644@users.noreply.github.com>
2021-09-09 16:20:26 +0700

    v3

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -13,7 +13,7 @@ const Comments = ({username, userImagePath, commentText, likeNum, replies} :Comm
         return (
             <div className='flex items-center'>
                 <img className='w-4 h-4 mr-1' src='/like.svg'></img>
-                <p className='text-gray-500'>{likeNum} คน</p>
+                <p className='text-gray-300'>{likeNum} คน</p>
             </div>
         )
     }
@@ -22,7 +22,7 @@ const Comments = ({username, userImagePath, commentText, likeNum, replies} :Comm
         <div>
             <div className="flex p-2 items-start space-x-2 text-white">
                 <img className="w-10 w-10 rounded-full" src={userImagePath}></img>
-                <div className="bg-gray-200 rounded-lg p-2">
+                <div className="bg-gray-500 rounded-lg p-2">
                 <p className="font-semibold text-white">{username}</p>
                 <p>{commentText}</p>
                 {/* like section (จะไม่แสดงถ้าไม่มีใครไลค์เลย) */}
