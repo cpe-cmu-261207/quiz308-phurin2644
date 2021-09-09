47f35f9edcaad0e98daeff99e86d27366cc385a4
phurin <76035456+phurin2644@users.noreply.github.com>
2021-09-09 16:21:10 +0700

    v3

--- src/components/Replies.tsx
+++ src/components/Replies.tsx
@@ -6,7 +6,7 @@ const Resplies = ({username, userImagePath, commentText, likeNum, replies} :Comm
         return (
             <div className='flex items-center'>
                 <img className='w-4 h-4 mr-1' src='/like.svg'></img>
-                <p className='text-gray-500'>{likeNum} คน</p>
+                <p className='text-gray-300'>{likeNum} คน</p>
             </div>
         )
     }
@@ -14,7 +14,7 @@ const Resplies = ({username, userImagePath, commentText, likeNum, replies} :Comm
     return (
         <div className="flex p-2 items-start space-x-2 pl-14 text-white">
             <img className="w-10 w-10 rounded-full" src={userImagePath}></img>
-            <div className="bg-gray-200 rounded-lg p-2">
+            <div className="bg-gray-500 rounded-lg p-2">
               <p className="font-semibold text-white">{username}</p>
               <p>{commentText}</p>
             {likeNum > 0 ? show() : null}
