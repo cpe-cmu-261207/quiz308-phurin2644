7ab87c9a58ad9d85383fdec7505162101ba15577
phurin <76035456+phurin2644@users.noreply.github.com>
2021-09-09 15:55:59 +0700

    Update Comment.tsx

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -20,10 +20,10 @@ const Comments = ({username, userImagePath, commentText, likeNum, replies} :Comm
 
     return (
         <div>
-            <div className="flex p-2 items-start space-x-2">
+            <div className="flex p-2 items-start space-x-2 text-white">
                 <img className="w-10 w-10 rounded-full" src={userImagePath}></img>
                 <div className="bg-gray-200 rounded-lg p-2">
-                <p className="font-semibold">{username}</p>
+                <p className="font-semibold text-white">{username}</p>
                 <p>{commentText}</p>
                 {/* like section (จะไม่แสดงถ้าไม่มีใครไลค์เลย) */}
                 {likeNum >= 0 ? show() : null} 
